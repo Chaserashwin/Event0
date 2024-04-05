0fabd6db1aec6fa48779c6f67e30d285cf775c9c
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-04-05 21:21:06 +0530

    update music

--- FRONTEND/src/pages/Event_Categories/Music.jsx
+++ FRONTEND/src/pages/Event_Categories/Music.jsx
@@ -18,35 +18,35 @@ const Music = () => {
           {/* Events Container */}
           <div className="w-full flex bg-slate-500">
             {/* Event Card */}
-            {categories?.map((categorie) => (
-              <div
-                key={developer?.id}
-                className="p-[8px] w-[25%] border-box border-2 border-black bg-pink-300"
-              >
-                <div className="flex flex-col h-[100%]">
-                  <div className="absolute top-0 left-0 h-[100%] w-[100%]"></div>
-                  {/* upperpart of card */}
-                  <a href="">
-                    <div>
-                      <img src="/banner/music.png" alt="" />
-                    </div>
-                  </a>
-                  {/* lowerpart of card */}
-                  <div className="pt-[8px] pr-[8px] pb-[16px] pl-[12px] ">
-                    <div className="space-[4px]">
-                      <a href="">
-                        <h2>chaserashwin - chasing life</h2>
-                      </a>
-                      <p>Thu,Apr 11 * 10:30 PM</p>
-                      <div className="mt-1"></div>
-                      <div className="flex flex-wrap items-center ">
-                        <p>Free</p>
-                      </div>
+            {/* {categories?.map((categorie) => ( */}
+            <div
+              // key={developer?.id}
+              className="p-[8px] w-[25%] border-box border-2 border-black bg-pink-300"
+            >
+              <div className="flex flex-col h-[100%]">
+                <div className="absolute top-0 left-0 h-[100%] w-[100%]"></div>
+                {/* upperpart of card */}
+                <a href="">
+                  <div>
+                    <img src="/banner/music.png" alt="" />
+                  </div>
+                </a>
+                {/* lowerpart of card */}
+                <div className="pt-[8px] pr-[8px] pb-[16px] pl-[12px] ">
+                  <div className="space-[4px]">
+                    <a href="">
+                      <h2>chaserashwin - chasing life</h2>
+                    </a>
+                    <p>Thu,Apr 11 * 10:30 PM</p>
+                    <div className="mt-1"></div>
+                    <div className="flex flex-wrap items-center ">
+                      <p>Free</p>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
+            </div>
+            {/* ))} */}
           </div>
         </div>
       </div>
