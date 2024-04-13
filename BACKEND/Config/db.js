a980f1d1df25a3100deb8701f60fbfeb45ccafa8
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2024-04-13 14:43:29 +0530

    Update db.js

--- BACKEND/Config/db.js
+++ BACKEND/Config/db.js
@@ -1,5 +1,5 @@
 var dotenv=require("dotenv").config();
 const mongoose=require('mongoose');
-mongoose.connect(process.env.MonGo_URL)
+mongoose.connect('mongodb+srv://chaserashwin:L9znbBuimFs8kI5I@projectdb.izmrs9k.mongodb.net/?retryWrites=true&w=majority&appName=Projectdb')
 .then(()=>{console.log("Database Connected successfully");})
 .catch((err)=>{console.log(err);})
